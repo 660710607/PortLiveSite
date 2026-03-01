d3813e440c3c24e929c193bb3e36db4ff912d791
660710607 <jangharn_p@silpakorn.edu>
2026-03-01 16:11:10 +0700

    Initial Commit

---
+++ js/script.js
@@ -0,0 +1,15 @@
+
+document.addEventListener('DOMContentLoaded', () => {
+
+    // --- Chart.js Data Viz - REMOVED ---
+
+    // Smooth scrolling for anchor links
+    document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+        anchor.addEventListener('click', function (e) {
+            e.preventDefault();
+            document.querySelector(this.getAttribute('href')).scrollIntoView({
+                behavior: 'smooth'
+            });
+        });
+    });
+});
